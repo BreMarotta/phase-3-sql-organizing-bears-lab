38cb1d9efd5590723102cf0ae69aa177cb7cd266
Breanne <BreMarotta@gmail.com>
2022-03-11 10:44:32 -0500

    commit tree bf06593e96f533b7caf03d831623ff08c730fd27

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "brown", "friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "F", "black", "aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 13, "F", "brown", "sassy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 16, "F", "brown", "sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "black", "cranky", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "black", NULL, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, NULL, NULL, NUll, 1);
